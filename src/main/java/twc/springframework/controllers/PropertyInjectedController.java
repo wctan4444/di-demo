a5ca2c59883ec50c0b703ac941697cf04b63239a
waichong <wctan4444@yahoo.com>
2019-03-23 22:18:45 +0800

    Not Using Spring Qualifer Annotation

--- src/main/java/twc/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/twc/springframework/controllers/PropertyInjectedController.java
@@ -2,16 +2,17 @@ package twc.springframework.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import twc.springframework.services.GreetingServiceImpl;
+import twc.springframework.services.GreetingService;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    //public GreetingServiceImpl greetingService;
+    public GreetingService greetingServiceImpl;
 
 
     public String sayHello(){
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 }
